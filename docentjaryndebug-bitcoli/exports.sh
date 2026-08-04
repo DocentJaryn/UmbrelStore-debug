6f311f84f50c94a015ce23e0f834ae43c26317d3
DocentJaryn <122935333+DocentJaryn@users.noreply.github.com>
2026-08-04 16:57:35 +0200

    Update exports.sh

--- docentjaryndebug-bitcoli/exports.sh
+++ docentjaryndebug-bitcoli/exports.sh
@@ -1,10 +1,10 @@
-export BITCOLI_UI_PORT="27618"
-export BITCOLI_API_PORT="27619"
-export BITCOLI_API_IP="10.21.21.89"
+export APP_BITCOLI_UI_PORT_DEBUG="27618"
+export APP_BITCOLI_API_PORT_DEBUG="27619"
+export APP_BITCOLI_API_IP_DEBUG="10.21.21.89"
 
 bitcoli_api_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-api/hostname"
 if [ -s "${bitcoli_api_hidden_service_file}" ]; then
-    export BITCOLI_API_HIDDEN_SERVICE="http://$(cat "${bitcoli_api_hidden_service_file}")"
+    export APP_BITCOLI_API_HIDDEN_SERVICE_DEBUG="http://$(cat "${bitcoli_api_hidden_service_file}")"
 else
-    export BITCOLI_API_HIDDEN_SERVICE=""
+    export APP_BITCOLI_API_HIDDEN_SERVICE_DEBUG=""
 fi
